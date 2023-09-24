2a24155344655191fbfeceb7fa2ffc9ac0988115
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-09-24 19:36:43 +0530

    new view 1

--- server/views.py
+++ server/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import requests
-
+import json
 def send_value_to_esp(request):
     if request.method == 'POST':
         value = request.POST.get('input')
-        print(value)
+        print("Value sent to ESP32:", value)
         
-        # Send a POST request to the ESP32 with the integer value
-        esp32_url = "http://192.168.148.175:80/receive-value" # Replace with your ESP32's IP and port
+        # Create a dictionary and convert it to JSON
         payload = {'value': value}
+        json_payload = json.dumps(payload)
+        print("JSON Payload:", json_payload)
+        
+        # Send a POST request to the ESP32 with the JSON payload
+        esp32_url = "http://192.168.148.175:80/receive-value"
         
         try:
-            response = requests.post(esp32_url, json=payload)
+            response = requests.post(esp32_url, data=json_payload, headers={'Content-Type': 'application/json'})
             if response.status_code == 200:
                 return JsonResponse({'message': 'Value sent to ESP32 successfully.'})
             else:
